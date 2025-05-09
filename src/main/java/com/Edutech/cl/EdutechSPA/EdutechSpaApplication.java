4f001ae52a8e3448adf12675c0f626ae0760a299
Melipilla <melipilla@duoc.acad>
2025-05-09 18:18:55 -0400

    Cambio de Ip

---
+++ src/main/java/com/Edutech/cl/EdutechSPA/EdutechSpaApplication.java
@@ -0,0 +1,13 @@
+package com.Edutech.cl.EdutechSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutechSpaApplication.class, args);
+	}
+
+}
